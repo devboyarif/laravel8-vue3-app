9ff526415c76c08ca4a6442d546826c38c3af587
devboyarif <arif14arif15@gmail.com>
2021-07-04 22:10:37 +0600

    vue router 4 integrate

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,16 @@
 require('./bootstrap');
 
 import { createApp } from 'vue';
-import App from './components/App.vue'
-const app = createApp({});
 
+import App from './components/App.vue'
+const app = createApp(App);
+
+// vue router use
+import router from './router/index.js'
+app.use(router)
+
+// component setup
 app.component('app', App);
+
+
 app.mount('#app');
--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,5 +1,8 @@
 <template>
 <h1>Welcome to Laravel8 with Vue 3 App</h1>
+<router-link class="btn btn-primary m-1" to="/">Home</router-link> &nbsp;&nbsp;&nbsp;
+<router-link class="btn btn-primary m-1" to="/about">About</router-link>
+<router-view></router-view>
 </template>
 
 <script>
