d3dc334a90bf835e6615010ae77611d5144f9707
devboyarif <arif14arif15@gmail.com>
2021-07-04 21:46:42 +0600

    laravel8 + vue3 setup

---
+++ resources/js/app.js
@@ -0,0 +1,8 @@
+require('./bootstrap');
+
+import { createApp } from 'vue';
+import App from './components/App.vue'
+const app = createApp({});
+
+app.component('app', App);
+app.mount('#app');
---
+++ resources/js/components/App.vue
@@ -0,0 +1,13 @@
+<template>
+<h1>Welcome to Laravel8 with Vue 3 App</h1>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
